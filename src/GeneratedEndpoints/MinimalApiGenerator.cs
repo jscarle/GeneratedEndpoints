866c7e58e507ba35f949a8851388851345bd6d6b
Jean-Sebastien Carle <29762210+jscarle@users.noreply.github.com>
2025-11-17 00:09:30 -0500

    Refactor.

--- src/GeneratedEndpoints/MinimalApiGenerator.cs
+++ src/GeneratedEndpoints/MinimalApiGenerator.cs
@@ -317,38 +317,4 @@ public sealed class MinimalApiGenerator : IIncrementalGenerator
             ArrayPool<char>.Shared.Return(buffer);
         }
     }
-
-    private readonly struct HandlerNameKey(string name, string method) : IEquatable<HandlerNameKey>
-    {
-        private readonly string _name = name;
-        private readonly string _method = method;
-        private readonly int _hashCode = CombineHashCodes(StringComparer.Ordinal.GetHashCode(name), StringComparer.Ordinal.GetHashCode(method));
-
-        public bool Equals(HandlerNameKey other)
-        {
-            return ReferenceEquals(_name, other._name) && ReferenceEquals(_method, other._method)
-                || (string.Equals(_name, other._name, StringComparison.Ordinal)
-                    && string.Equals(_method, other._method, StringComparison.Ordinal));
-        }
-
-        public override bool Equals(object? obj)
-        {
-            return obj is HandlerNameKey other && Equals(other);
-        }
-
-        public override int GetHashCode()
-        {
-            return _hashCode;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static int CombineHashCodes(int left, int right)
-        {
-            unchecked
-            {
-                return (left * 397) ^ right;
-            }
-        }
-    }
-
 }
